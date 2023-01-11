b9c52c1e3b4aec48d551b216e1179f686236470a
vishal101022 <raivishal939@gmail.com>
2023-01-11 23:13:17 +0530

    :sparkles: added new products

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const dummyProduct = [
+  {
+    id: 1,
+    price: 24,
+    title: 'book',
+    description: 'friction book'
+  },
+  {
+    id: 2,
+    price: 5,
+    title: 'pen',
+    description: 'ball pen with refill'
+  }
+
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {dummyProduct.map(product =>(
+            <ProductItem
+            key={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );

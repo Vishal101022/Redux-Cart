155b7cc453255c3f9cad7456c256beed27144d54
vishal101022 <raivishal939@gmail.com>
2023-01-12 19:27:50 +0530

    :sparkles: added quantity number badge

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,18 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { uiActions } from "../store/uiSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+ const cartQuantity = useSelector(state => state.cart.totalQuantity);
+
+  const toggleCartHadler = () => {
+    dispatch(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHadler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
